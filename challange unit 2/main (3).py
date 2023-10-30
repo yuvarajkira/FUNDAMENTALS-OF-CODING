bc87ac1612bbd42f3b54029474f5c668d399b7bf
yuvarajkira <149384652+yuvarajkira@users.noreply.github.com>
2023-10-30 17:24:03 +0530

    Add files via upload

---
+++ challange unit 2/main (3).py
@@ -0,0 +1,18 @@
+class Base(object):
+    pass   # Empty Class
+  
+class Derived(Base):
+    pass   # Empty Class
+  
+# Driver Code
+print(issubclass(Derived, Base))
+print(issubclass(Base, Derived))
+  
+d = Derived()
+b = Base()
+  
+# b is not an instance of Derived
+print(isinstance(b, Derived))
+  
+# But d is an instance of Base
+print(isinstance(d, Base))
